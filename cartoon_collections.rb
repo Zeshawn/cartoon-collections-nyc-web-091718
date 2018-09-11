76d2710ade66a257a0d6ea8771f3822f44b93f4a
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-11 00:54:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index do | 
+  dwarves.each_with_index do | names, index|
+    
 end
 
 def summon_captain_planet# code an argument here
