a4af8eb7cce123c874172a769d6988ff29c71293
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-11 01:06:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,8 @@ end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
 end
 
 def long_planeteer_calls# code an argument here
