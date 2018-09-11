fa0d8d719938bda0dd0e8c19bb754ad24b1a9fc7
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-11 00:58:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(dwarves)# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
-  dwarves.each_with_index do |names, index|
+  array.each_with_index do |names, index|
     index +=1
     puts "#{index}. #{name}"
   end
